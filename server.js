f238147841472987abbbf114f2808562bf82e8a6
raulglezrdguez <raulglezrguez@gmail.com>
2022-09-07 10:48:35 -0400

    server updated

--- server.js
+++ server.js
@@ -3,3 +3,41 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 require('dotenv').config();
+
+const expressMidleware = require('./util/expressMiddleware');
+
+let mongoUrl = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_IP}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}`;
+if (process.env.MONGO_USER === '') {
+  mongoUrl = `mongodb://${process.env.MONGO_IP}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}`;
+}
+
+const authRouter = require('./routes/userRoutes');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const connetWithRetry = () => {
+  mongoose
+    .connect(mongoUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    })
+    .then(() => console.log('Connected to database'))
+    .catch((e) => {
+      console.log(e);
+      setTimeout(connetWithRetry, 5000);
+    });
+};
+
+connetWithRetry();
+
+app.use(expressMidleware);
+
+app.use('/auth', authRouter);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`backend ready on port: ${port}`));

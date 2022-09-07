6788d386db2c038e80db4898302abb78e9563ad2
raulglezrdguez <raulglezrguez@gmail.com>
2022-09-07 15:47:36 -0400

    auth login route added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.post('/signup', authController.signUp);
 router.post('/activate', authController.activate);
+router.post('/login', authController.login);
 router.post('/forgotpass', authController.forgotpass);
 router.post('/recoverypass', authController.recoverypass);
 

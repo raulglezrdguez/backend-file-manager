a9d9f3f1264813736e96d221594dd5e61e8d4acc
raulglezrdguez <raulglezrguez@gmail.com>
2022-09-07 09:26:14 -0400

    transporter created

---
+++ util/transporter.js
@@ -0,0 +1,45 @@
+const nodemailer = require('nodemailer');
+const dotenv = require('dotenv');
+dotenv.config({ path: '../.env' });
+
+// // create reusable transporter object using the default SMTP transport
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.gmail.com",
+//   port: 465,
+//   secure: true, // true for 465, false for other ports
+//   auth: {
+//     user: "shareta2022@gmail.com",
+//     pass: "gvcacclkdexchsrc",
+//   },
+// });
+
+// transporter
+//   .verify()
+//   .then(() => {
+//     console.log("ready for send emails");
+//   })
+//   .catch((err) => console.log(err));
+
+// module.exports = transporter;
+
+const transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  // authMethod:'NTLM',
+  secure: false, // upgrade later with STARTTLS
+  tls: { rejectUnauthorized: false },
+  // debug:true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
+
+transporter
+  .verify()
+  .then(() => {
+    console.log('ready for send emails');
+  })
+  .catch((err) => console.log(err));
+
+module.exports = transporter;

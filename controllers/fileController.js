44dc2397e3260a7af5292d9c6f152edc40af73fe
raulglezrdguez <raulglezrguez@gmail.com>
2022-09-08 11:36:42 -0400

    model File save asyn added

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -6,35 +6,41 @@ const File = require('../models/fileModel');
 const Status = require('../util/fileStatus');
 
 exports.fileupload = async (req, res) => {
-    try {
-        const form = new formidable.IncomingForm();
-      
-        form.parse(req, function (err, fields, files) {
-          const oldpath = files.filetoupload.filepath;
-          let file = new File({
-            name: files.filetoupload.originalFilename,
-            status: Status.Uploaded,
-            body: fs.readFileSync(oldpath),
-          });
-          file = await file.save();
-      
+  try {
+    const form = new formidable.IncomingForm();
+
+    form.parse(req, function (err, fields, files) {
+      const oldpath = files.filetoupload.filepath;
+      let file = new File({
+        name: files.filetoupload.originalFilename,
+        status: Status.Uploaded,
+        body: fs.readFileSync(oldpath),
+      });
+      file
+        .save()
+        .then((file) => {
           return res.send({ message: JSON.stringify(fields) });
         })
-    } catch(err) {
-        return res.status(500).send({ general: 'Internal server error' });
-    }
-    // const newpath = path.join(
-    //   __dirname,
-    //   '/../',
-    //   '/files/',
-    //   files.filetoupload.originalFilename
-    // );
-    // fs.rename(oldpath, newpath, function (err) {
-    //   if (err) {
-    //     console.log(err);
-    //     return res.status(400).send({ error: err });
-    //   }
-    //   return res.send({ message: 'File uploaded and moved' });
-    // });
-  
+        .catch((err) => {
+          return res
+            .status(400)
+            .send({ general: 'Server error: saving file document' });
+        });
+    });
+  } catch (err) {
+    return res.status(500).send({ general: 'Internal server error' });
+  }
+  // const newpath = path.join(
+  //   __dirname,
+  //   '/../',
+  //   '/files/',
+  //   files.filetoupload.originalFilename
+  // );
+  // fs.rename(oldpath, newpath, function (err) {
+  //   if (err) {
+  //     console.log(err);
+  //     return res.status(400).send({ error: err });
+  //   }
+  //   return res.send({ message: 'File uploaded and moved' });
+  // });
 };

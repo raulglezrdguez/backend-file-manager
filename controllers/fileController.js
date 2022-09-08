20dae5e1ea34949d18ba57330712bbbb8b61931b
raulglezrdguez <raulglezrguez@gmail.com>
2022-09-08 10:54:38 -0400

    fileController created

---
+++ controllers/fileController.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const formidable = require('formidable');
+
+const File = require('../models/fileModel');
+const Status = require('../util/fileStatus');
+
+exports.fileupload = async (req, res) => {
+  const form = new formidable.IncomingForm();
+
+  form.parse(req, function (err, fields, files) {
+    const oldpath = files.filetoupload.filepath;
+    const newpath = 'C:/Users/Your Name/' + files.filetoupload.originalFilename;
+    fs.rename(oldpath, newpath, function (err) {
+      if (err) {
+        console.log(err);
+        return res.status(400).send({ error: err });
+      }
+      return res.send({ message: 'File uploaded and moved' });
+    });
+  });
+};

bdb3e06e6d482ab9acd0841c8bd0347a5d35a84b
raulglezrdguez <raulglezrguez@gmail.com>
2022-09-08 11:32:08 -0400

    model File added

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -6,22 +6,35 @@ const File = require('../models/fileModel');
 const Status = require('../util/fileStatus');
 
 exports.fileupload = async (req, res) => {
-  const form = new formidable.IncomingForm();
-
-  form.parse(req, function (err, fields, files) {
-    const oldpath = files.filetoupload.filepath;
-    const newpath = path.join(
-      __dirname,
-      '/../',
-      '/files/',
-      files.filetoupload.originalFilename
-    );
-    fs.rename(oldpath, newpath, function (err) {
-      if (err) {
-        console.log(err);
-        return res.status(400).send({ error: err });
-      }
-      return res.send({ message: 'File uploaded and moved' });
-    });
-  });
+    try {
+        const form = new formidable.IncomingForm();
+      
+        form.parse(req, function (err, fields, files) {
+          const oldpath = files.filetoupload.filepath;
+          let file = new File({
+            name: files.filetoupload.originalFilename,
+            status: Status.Uploaded,
+            body: fs.readFileSync(oldpath),
+          });
+          file = await file.save();
+      
+          return res.send({ message: JSON.stringify(fields) });
+        })
+    } catch(err) {
+        return res.status(500).send({ general: 'Internal server error' });
+    }
+    // const newpath = path.join(
+    //   __dirname,
+    //   '/../',
+    //   '/files/',
+    //   files.filetoupload.originalFilename
+    // );
+    // fs.rename(oldpath, newpath, function (err) {
+    //   if (err) {
+    //     console.log(err);
+    //     return res.status(400).send({ error: err });
+    //   }
+    //   return res.send({ message: 'File uploaded and moved' });
+    // });
+  
 };

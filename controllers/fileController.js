b93ca4a1f642de4f149cfb601dded834702a975e
raulglezrdguez <raulglezrguez@gmail.com>
2022-09-09 10:00:44 -0400

    user authentication added

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -2,61 +2,95 @@ const fs = require('fs').promises;
 const formidable = require('formidable');
 
 const File = require('../models/fileModel');
+const User = require('../models/userModel');
 const Status = require('../util/fileStatus');
 
 exports.fileupload = async (req, res) => {
-  try {
-    const form = new formidable.IncomingForm();
+  if (req && req.user && req.user.id) {
+    const userId = req.user.id;
+    try {
+      // Confirm user does exist
+      const userDB = await User.findById(userId);
+      if (!userDB) {
+        return res.status(400).send({ general: 'User does not exists' });
+      }
 
-    form.parse(req, function (err, fields, files) {
-      const oldpath = files.filetoupload.filepath;
-      fs.readFile(oldpath)
-        .then((body) => {
-          let file = new File({
-            name: fields.name,
-            status: Status.Uploaded,
-            body: Buffer.from(body),
-            originalFileName: files.filetoupload.originalFilename,
-          });
-          file
-            .save()
-            .then((file) => {
-              return res.send({ message: JSON.stringify(fields) });
-            })
-            .catch((err) => {
-              return res
-                .status(400)
-                .send({ general: 'Server error: saving file document' });
+      const form = new formidable.IncomingForm();
+
+      form.parse(req, function (err, fields, files) {
+        if (err) {
+          return res.status(400).send({ general: 'Error parsing form' });
+        }
+        const oldpath = files.filetoupload.filepath;
+        fs.readFile(oldpath)
+          .then((body) => {
+            let file = new File({
+              owner: userId,
+              name: fields.name,
+              status: Status.Uploaded,
+              body: Buffer.from(body),
+              originalFileName: files.filetoupload.originalFilename,
             });
-        })
-        .catch((err) => {
-          return res
-            .status(400)
-            .send({ general: 'Server error: reading file' });
-        });
-    });
-  } catch (err) {
-    return res.status(500).send({ general: 'Internal server error' });
+            file
+              .save()
+              .then((file) => {
+                return res.send({ message: 'file uploaded' });
+              })
+              .catch((err) => {
+                return res
+                  .status(400)
+                  .send({ general: 'Server error: saving file document' });
+              });
+          })
+          .catch((err) => {
+            return res
+              .status(400)
+              .send({ general: 'Server error: reading file' });
+          });
+      });
+    } catch (err) {
+      return res.status(500).send({ general: 'Internal server error' });
+    }
+  } else {
+    return res.status(401).send({ general: 'User not logged in' });
   }
 };
 
 exports.filedownload = async (req, res, next) => {
-  if (req && req.query && req.query.fileId) {
-    const { fileId } = req.query;
+  if (req && req.user && req.user.id) {
+    const userId = req.user.id;
 
     try {
-      const fileDB = await File.findById(fileId);
-      if (!fileDB || fileDB.status !== Status.Zipped) {
-        return res.status(400).send({ fileId: 'Not found or not compressed' });
+      // Confirm user does exist
+      const userDB = await User.findById(userId);
+      if (!userDB) {
+        return res.status(400).send({ general: 'User does not exists' });
       }
-      res.setHeader('Content-Type', 'application/zip'); // .zip    application/zip
-      return res.status(200).send(fileDB.body);
-    } catch (err) {
-      console.log(err.message);
 
-      return res.status(500).send({ general: 'Database error' });
+      if (req.query && req.query.fileId) {
+        const { fileId } = req.query;
+
+        try {
+          const fileDB = await File.findById(fileId);
+          if (!fileDB || fileDB.status !== Status.Zipped) {
+            return res
+              .status(400)
+              .send({ fileId: 'Not found or not compressed' });
+          }
+          res.setHeader('Content-Type', 'application/zip'); // .zip    application/zip
+          return res.status(200).send(fileDB.body);
+        } catch (err) {
+          console.log(err.message);
+
+          return res.status(500).send({ general: 'Database error' });
+        }
+      } else {
+        return res.status(500).send({ general: 'Param fileId needed' });
+      }
+    } catch (err) {
+      return res.status(500).send({ general: 'Internal server error' });
     }
   } else {
-    return res.status(500).send({ general: 'Param fileId needed' });
+    return res.status(401).send({ general: 'User not logged in' });
   }
 };

bde27f1dff700aa578dbf39f88d91c8d61a6182e
raulglezrdguez <raulglezrguez@gmail.com>
2022-09-11 15:32:46 -0400

    google service added

--- util/transporter.js
+++ util/transporter.js
@@ -26,8 +26,8 @@ const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: process.env.EMAIL_PORT,
   // authMethod:'NTLM',
-  secure: false, // upgrade later with STARTTLS
-  tls: { rejectUnauthorized: false },
+  secure: process.env.EMAIL_PORT === 465 ? true : false, // upgrade later with STARTTLS
+  // tls: { rejectUnauthorized: false },
   // debug:true,
   auth: {
     user: process.env.EMAIL_USER,

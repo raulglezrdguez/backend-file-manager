2695d7d484b7219d0e46207e88f369a2d18dafc7
raulglezrdguez <raulglezrguez@gmail.com>
2022-09-11 16:23:59 -0400

    comments deleted

--- util/transporter.js
+++ util/transporter.js
@@ -2,26 +2,6 @@ const nodemailer = require('nodemailer');
 const dotenv = require('dotenv');
 dotenv.config({ path: '../.env' });
 
-// // create reusable transporter object using the default SMTP transport
-// const transporter = nodemailer.createTransport({
-//   host: "smtp.gmail.com",
-//   port: 465,
-//   secure: true, // true for 465, false for other ports
-//   auth: {
-//     user: "shareta2022@gmail.com",
-//     pass: "gvcacclkdexchsrc",
-//   },
-// });
-
-// transporter
-//   .verify()
-//   .then(() => {
-//     console.log("ready for send emails");
-//   })
-//   .catch((err) => console.log(err));
-
-// module.exports = transporter;
-
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: process.env.EMAIL_PORT,

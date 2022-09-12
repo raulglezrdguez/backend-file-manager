f2bca85232d376b5d88dc2e4409196fc592c7547
raulglezrdguez <raulglezrdguez69@gmail.com>
2022-09-12 09:17:04 -0400

    service gmail added to fix connection error

--- util/transporter.js
+++ util/transporter.js
@@ -3,12 +3,13 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "../.env" });
 
 const transporter = nodemailer.createTransport({
+  service: "Gmail",
   host: process.env.EMAIL_HOST,
   port: process.env.EMAIL_PORT,
   // authMethod:'NTLM',
   secure: process.env.EMAIL_PORT === 465 ? true : false, // upgrade later with STARTTLS
   // tls: { rejectUnauthorized: false },
-  // debug:true,
+  debug: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,

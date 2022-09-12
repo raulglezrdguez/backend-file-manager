f2df039ba6bb29e63bec78269f0e0d3db6ef674b
raulglezrdguez <raulglezrdguez69@gmail.com>
2022-09-12 08:50:28 -0400

    updated to deployment

--- util/transporter.js
+++ util/transporter.js
@@ -1,13 +1,13 @@
-const nodemailer = require('nodemailer');
-const dotenv = require('dotenv');
-dotenv.config({ path: '../.env' });
+const nodemailer = require("nodemailer");
+const dotenv = require("dotenv");
+dotenv.config({ path: "../.env" });
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: process.env.EMAIL_PORT,
   // authMethod:'NTLM',
   secure: process.env.EMAIL_PORT === 465 ? true : false, // upgrade later with STARTTLS
-  tls: { rejectUnauthorized: false },
+  // tls: { rejectUnauthorized: false },
   // debug:true,
   auth: {
     user: process.env.EMAIL_USER,
@@ -18,7 +18,7 @@ const transporter = nodemailer.createTransport({
 transporter
   .verify()
   .then(() => {
-    console.log('ready for send emails');
+    console.log("ready for send emails");
   })
   .catch((err) => console.log(err));
 

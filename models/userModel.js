29b0347d29af5bc2f1e231d9db7cc1f4feaee67d
raulglezrdguez <raulglezrdguez69@gmail.com>
2022-10-17 10:27:54 -0400

    added set toJSON

--- models/userModel.js
+++ models/userModel.js
@@ -25,6 +25,14 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id;
+    // delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
--- server.js
+++ server.js
@@ -1,30 +1,30 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const cron = require('node-cron')
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const cron = require('node-cron');
 
-require('dotenv').config()
+require('dotenv').config();
 
-const expressMidleware = require('./util/expressMiddleware')
-const { zipFiles } = require('./util/zipFile')
+const expressMidleware = require('./util/expressMiddleware');
+const { zipFiles } = require('./util/zipFile');
 
 // let mongoUrl = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.2xsawxs.mongodb.net/?retryWrites=true&w=majority`;
-let mongoUrl = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@ac-z7qx0rf-shard-00-00.2xsawxs.mongodb.net:27017,ac-z7qx0rf-shard-00-01.2xsawxs.mongodb.net:27017,ac-z7qx0rf-shard-00-02.2xsawxs.mongodb.net:27017/?ssl=true&replicaSet=atlas-bbfw2s-shard-0&authSource=admin&retryWrites=true&w=majority`
+let mongoUrl = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@ac-z7qx0rf-shard-00-00.2xsawxs.mongodb.net:27017,ac-z7qx0rf-shard-00-01.2xsawxs.mongodb.net:27017,ac-z7qx0rf-shard-00-02.2xsawxs.mongodb.net:27017/?ssl=true&replicaSet=atlas-bbfw2s-shard-0&authSource=admin&retryWrites=true&w=majority`;
 if (process.env.MONGO_USER === '') {
   const db =
     process.env.NODE_ENV === 'test'
       ? process.env.MONGO_DATABASE_TEST
-      : process.env.MONGO_DATABASE
-  mongoUrl = `mongodb://${process.env.MONGO_IP}:${process.env.MONGO_PORT}/${db}`
+      : process.env.MONGO_DATABASE;
+  mongoUrl = `mongodb://${process.env.MONGO_IP}:${process.env.MONGO_PORT}/${db}`;
 }
 
-const authRouter = require('./routes/userRoutes')
-const fileRouter = require('./routes/fileRoutes')
+const authRouter = require('./routes/userRoutes');
+const fileRouter = require('./routes/fileRoutes');
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 const connetWithRetry = () => {
   mongoose
@@ -34,36 +34,36 @@ const connetWithRetry = () => {
     })
     .then(() => console.log('Connected to database'))
     .catch((e) => {
-      console.log(e)
-      setTimeout(connetWithRetry, 5000)
-    })
-}
+      console.log(e);
+      setTimeout(connetWithRetry, 5000);
+    });
+};
 
-connetWithRetry()
+connetWithRetry();
 
-app.use(expressMidleware)
+app.use(expressMidleware);
 
 app.get('/', (req, res) => {
-  res.send('<h2>Hello backend users</h2>')
-})
+  res.send('<h2>Hello backend users</h2>');
+});
 
-app.use('/auth', authRouter)
-app.use('/file', fileRouter)
+app.use('/auth', authRouter);
+app.use('/file', fileRouter);
 
 app.use((req, res) => {
   res.status(404).json({
     error: 'Not found',
-  })
-})
+  });
+});
 
 cron.schedule('*/1 * * * *', () => {
-  zipFiles()
-})
+  zipFiles();
+});
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
 
-app.listen(port, () => console.log(`backend ready on port: ${port}`))
+app.listen(port, () => console.log(`backend ready on port: ${port}`));
 
-module.export = {
+module.exports = {
   app,
-}
+};

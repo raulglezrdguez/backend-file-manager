1631d96e9a149012f8ac128334e95601bb24fffb
raulglezrdguez <raulglezrdguez69@gmail.com>
2022-10-18 08:47:18 -0400

    commented nodemailer mock

--- tests/user.test.js
+++ tests/user.test.js
@@ -4,28 +4,29 @@ const mongoose = require('mongoose');
 // const User = require('../models/userModel');
 const { app, server } = require('../server');
 
-const nodemailer = require('nodemailer');
+// const nodemailer = require('nodemailer');
 
-const sendMailMock = jest.fn();
-const verifyMock = jest.fn().mockReturnValue({});
+// const sendMailMock = jest.fn();
+// const verifyMock = jest.fn().mockReturnValue({});
 
-jest.mock('nodemailer');
-nodemailer.createTransport.mockReturnValue({
-  sendMail: sendMailMock,
-  verify: verifyMock,
-});
+// jest.mock('nodemailer');
+// nodemailer.createTransport.mockReturnValue({
+//   sendMail: sendMailMock,
+//   verify: verifyMock,
+// });
 
 const api = supertest(app);
 
 beforeEach(async () => {
   // await User.deleteMany({})
-  sendMailMock.mockClear();
-  nodemailer.createTransport.mockClear();
+  // sendMailMock.mockClear();
+  // nodemailer.createTransport.mockClear();
 });
 
 test('login return json', async () => {
   await api
     .post('/auth/login')
+    .send({ user: 'test', password: 'test' })
     .expect(400)
     .expect('Content-Type', /application\/json/);
 });
